dd91b9d4dd7d9226b63001ef2b9729e312221eb2
Mathzilla <teixeira.matheus26@gmail.com>
2023-06-30 19:55:22 -0300

    added animation to about section

--- components/About.tsx
+++ components/About.tsx
@@ -2,11 +2,12 @@
 import React from "react";
 const localUrl = process.env.NEXT_PUBLIC_BACKEND_URL
 import { motion, useScroll, useTransform } from "framer-motion";
-
+import Lottie from 'lottie-react'
 import login from "../public/login.png";
 import heart from "../public/heart.png";
 import quality from "../public/quality.png";
 import responsive from "../public/educational-programs.png";
+import animationData from '../public/animation.json'
 import AboutImage from "./AboutImage";
 
 type Props = {
@@ -18,8 +19,6 @@ type Props = {
 export default function About({ title, text, icons }: Props) {
   console.log(icons)
   return (
-    <div className="h-screen w-full flex items-center justify-start">
-      <AboutImage />
       <motion.div
         initial={{
           opacity: 0,
@@ -30,12 +29,15 @@ export default function About({ title, text, icons }: Props) {
         transition={{
           duration: 1.5,
         }}
-        className="relative w-full lg:w-[50%] p-0 h-full rounded-md flex justify-center items-center" 
+        className="relative w-full p-0 h-screen rounded-md flex justify-center items-center" 
       >
         <div className="h-[90%] flex flex-col overflow-y-hidden justify-center items-center">
           <h3 className=" text-gray-500 mb-10 tracking-[20px] uppercase text-[12px] md:text-2xl text-center">
             {title}
           </h3>
+          <div className='w-[50%]'>
+      <Lottie animationData={animationData}/>
+      </div>
           <div className="flex w-[100%] h-[80%] justify-center items-center flex-wrap">
             {icons?.map((icon:any, index:number)=>{
               return(
@@ -49,6 +51,5 @@ export default function About({ title, text, icons }: Props) {
           </div>
         </div>
       </motion.div>
-    </div>
   );
 }
