3a9b5c0cf15d0ae578a23f55360a8121e74ad46f
Mathzilla <teixeira.matheus26@gmail.com>
2023-07-03 19:55:03 -0300

    changed languagesBar size

--- components/LanguagesBar.tsx
+++ components/LanguagesBar.tsx
@@ -8,7 +8,7 @@ type Props = {setT:any}
 export default function LanguagesBar({setT}: Props) {
 
   return (
-    <div className={'w-full h-screen fixed top-0 flex justify-center m-auto pointer-events-none z-50'}>
+    <div className={'w-full h-[100dvh] fixed top-0 flex justify-center m-auto pointer-events-none z-50'}>
             <div className='max-w-7xl w-full flex flex-col justify-end items-start xl:flex-row xl:items-end xl:justify-start'>
                 <div className='mr-[32px] mb-[32px] space-y-2 space-x-0 xl:space-x-2 xl:space-y-0'>
                 <img draggable={false} onClick={()=>setT('pt')} className='pointer-events-auto w-10 block xl:inline saturate-0 hover:saturate-100 hover:scale-[1.2]' src={portugueseFlag.src} alt="portuguese" />
